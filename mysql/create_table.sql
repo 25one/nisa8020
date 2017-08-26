5172cc9fe6ece58802b89d8770f3dade8d97b055
25one <25one@ukr.net>
2017-08-26 22:08:17 +0300

    Some commit message

---
+++ mysql/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `#__citis` (
+	`id` int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`title` varchar(100) NOT NULL
+);
+
+INSERT INTO `#__citis` (`title`) VALUES ('Москва');
+INSERT INTO `#__citis` (`title`) VALUES ('Санкт-Петербург');
